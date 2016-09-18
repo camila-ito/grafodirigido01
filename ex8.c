b39acbd76e425e86a743de3b5e320ab041dc5df8
Camila Ito Dos Santos <camiladjuito@gmail.com>
2016-09-18 11:37:31 -0300

    scanf da terceira pergunta da entrada de dados

--- ex8.c
+++ ex8.c
@@ -68,7 +68,7 @@
       printf("digite o valor y do circulo1\n");
       scanf("%lf",&y_circulo1);
       printf("digite o valor do x do circulo2\n");
-
+      scanf("%lf",&x_circulo2_);
       distancia = sqrt(pow(x_circulo2 - x_circulo1,2)+pow(y_circulo2 - y_circulo1,2)); 
       /*distacia entre os dois centros*/ 
 
