59c24dd70458bf79fd86af5df7d596d838cf62ea
Victor Cajuba de Britto Bacelar Caldas <victorcajuba@gmail.com>
2016-09-25 12:21:12 -0300

    adicionada linha de codigo que pega o y medio entre v[1] e v[7]

--- ex9.c
+++ ex9.c
@@ -210,6 +210,7 @@ int seta(BITMAP *buff, float x_circ1, float y_circ1, float x_circ2, float y_circ
     xa = (v[0] + v[6])/2;
     y = (v[1] + v[7])/2;
     xd = v[0] - v[6];
+    yd = v[1] - v[7];
 
 
 
