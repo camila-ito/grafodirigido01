049cc82f6b68bc77ec9ef99f430d06d5824ec67d
Camila Ito Dos Santos <camiladjuito@gmail.com>
2016-09-25 09:58:16 -0300

    funcao seta: calculo da distancia entre os dois centros e declaracao de variaveis

--- ex9.c
+++ ex9.c
@@ -184,10 +184,13 @@ int quant_graf(char letra[][2], char *graf, int i)
 int seta(BITMAP *buff, float x_circ1, float y_circ1, float x_circ2, float y_circ2)
 {   int/* r,*/v[8], xa, y, xd, yd;
     float  A,x1,x2,x3,y1,y2,y3,sent,cost;/*circunferencia1*/
+    float  seno1,seno2,cosseno1,cosseno2,distancia1,distancia2;/*d,xm,ym,*/
     int xmedio,ymedio;/*ponto medio entre elas*/
     
     xmedio = (x_circ1+x_circ2)/2;
     ymedio = (y_circ1+y_circ2)/2;
+
+    distancia2=sqrt(pow(xmedio - x_circ2,2)+pow(ymedio - y_circ2,2)); /*distacia entre os dois centros*/ 
 
     distancia1=sqrt(pow(xmedio - x_circ1,2)+pow(ymedio - y_circ1,2)); /*distacia entre os dois centros*/ 
 
