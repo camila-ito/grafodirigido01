5cf308eac93022611b20e9fdab96ba84edc2e1c2
Victor Cajuba de Britto Bacelar Caldas <victorcajuba@gmail.com>
2016-09-25 12:19:25 -0300

    adicionada linha de codigo que sera responsavel por gerar o delta y entre os pontos v[1] e v[7] para futuramente gerar os pontos v[5]e v[3]

--- ex9.c
+++ ex9.c
@@ -208,6 +208,8 @@ int seta(BITMAP *buff, float x_circ1, float y_circ1, float x_circ2, float y_circ
     v[6]=x_circ2+20*cosseno2;
     v[7]=y_circ2+20*seno2;
     xa = (v[0] + v[6])/2;
+    y = (v[1] + v[7])/2;
+
 
 
 
