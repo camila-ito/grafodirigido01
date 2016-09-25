de7a09f310101a5e9475b840cbab847e02266552
Camila Ito Dos Santos <camiladjuito@gmail.com>
2016-09-25 09:55:51 -0300

    funcao seta: calculo do ponto medio

--- ex9.c
+++ ex9.c
@@ -184,6 +184,11 @@ int quant_graf(char letra[][2], char *graf, int i)
 int seta(BITMAP *buff, float x_circ1, float y_circ1, float x_circ2, float y_circ2)
 {   int/* r,*/v[8], xa, y, xd, yd;
     float  A,x1,x2,x3,y1,y2,y3,sent,cost;/*circunferencia1*/
+    int xmedio,ymedio;
+    
+    xmedio = (x_circ1+x_circ2)/2;
+    ymedio = (y_circ1+y_circ2)/2;
+
 
     return 0;
 }
