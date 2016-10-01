8494c34973c2759be5c20d84e1b82827b3d08596
Daniel De Oliveira Silva <dnl.oliveira11@gmail.com>
2016-10-01 18:07:21 -0300

    variaveis que serao utilizadas, algumas com base no ex9

--- ex10.c
+++ ex10.c
@@ -4,6 +4,11 @@
 
 int main(int argc, char *argv[])
 {
+    int i, r, opt,
+        j;
+    char letra[20][2], 
+         graf[20], ref[2],
+         url[100]= "entrada.txt";
     
     return EXIT_SUCCESS;
 }
