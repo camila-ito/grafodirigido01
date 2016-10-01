664fb1ab58a9ffa8bca02e92f778b69210104920
Daniel De Oliveira Silva <dnl.oliveira11@gmail.com>
2016-10-01 18:16:17 -0300

    dox e comentarios

--- ex10.c
+++ ex10.c
@@ -2,10 +2,20 @@
 #include<math.h>
 #define DEBUG 0
 
+
+/* --------------------------------------------------------------------------*/
+/**
+ * \brief 
+ *
+ * \param argc
+ * \param argv[]
+ *
+ * \return 
+ */
 int main(int argc, char *argv[])
 {
     int i, r, opt,
-        j;
+        j; /*numero de automatos*/
     char letra[20][2], 
          graf[20], ref[2],
          url[100]= "entrada.txt";
@@ -27,6 +37,8 @@ int main(int argc, char *argv[])
                 printf("Type\n\t$man %s\nor\n\t$%s -h\nfor help.\n\n", argv[0], argv[0]);
                 return EXIT_FAILURE;
         }  
-
+    
+    /*começo do codigo*/
+     
     return EXIT_SUCCESS;
 }
