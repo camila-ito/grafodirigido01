b178a1e931f47d830ba5069cf105a6dcd583ffcf
Rafael Santos Pereira Da Silva <rafappr99@gmail.com>
2016-10-01 23:36:49 -0300

    add template

--- ex10.c
+++ ex10.c
@@ -1,3 +1,33 @@
+/* ************************************************************************ *
+ *    ex10.x, VERSION 1.0                                                   *
+ *    Identificar Caminho, Circuito ou N/A                                  *
+ *                                                                          *
+ *    Copyright (C) 2016 by Grupo 1                                         *
+ * ************************************************************************ *
+ *                                                                          *
+ *    This program is free software; you can redistribute it and/or modify  *
+ *    it under the terms of the GNU General Public License as published by  *
+ *    the Free Software Foundation; either version 2 of the License, or     *
+ *    (at your option) any later version.                                   *
+ *                                                                          *
+ *    This program is distributed in the hope that it will be useful,       *
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *    GNU General Public License for more details.                          *
+ *                                                                          *
+ *    You should have received a copy of the GNU General Public License     *
+ *    along with this program; if not, write to the                         *
+ *    Free Software Foundation, Inc.,                                       *
+ *    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                          *
+ * ************************************************************************ *
+ *    To contact the author, please write to:                               *
+ *    Rafael Santos Pereira da Silva <rafappr99@gmail.com>                  *
+ *    Webpage: http://www.poli.br                                           *
+ *    Phone: +55 (81) 99345-0341                                            *
+ * ************************************************************************ *
+ * 
+ */
 #define MODULO(x) ((x)>=0 ? (x) : (-1*(x)))
 #define PI 3.14159265
 
